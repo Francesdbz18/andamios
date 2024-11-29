ee3465fbf30d4b8a01d580506083ada9d8a5caf6
Francesco Fevoli <francesdbz18pc@outlook.es>
2024-11-29 12:06:51 +0100

    lazyrow

--- app/src/main/java/com/chesco/andamios/MainActivity.kt
+++ app/src/main/java/com/chesco/andamios/MainActivity.kt
@@ -18,11 +18,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
@@ -30,6 +30,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -39,28 +40,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.chesco.andamios.ui.theme.AppTheme
-
 
 
 class MainActivity : ComponentActivity() {
@@ -71,70 +64,6 @@ class MainActivity : ComponentActivity() {
             AppTheme {
                 Andamio()
             }
-        }
-    }
-}
-
-@Composable
-fun MainScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ScaffoldExample() {
-    var presses by remember { mutableIntStateOf(0) }
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = colorScheme.primaryContainer,
-                    titleContentColor =  colorScheme.primary,
-                ),
-                title = {
-                    Text("Top app bar")
-                }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = colorScheme.primaryContainer,
-                contentColor = colorScheme.primary,
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    text = "Bottom app bar",
-                )
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { presses++ }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text =
-                """
-                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
-
-                    It also contains some basic inner content, such as this text.
-
-                    You have pressed the floating action button $presses times.
-                """.trimIndent(),
-            )
         }
     }
 }
@@ -168,15 +97,17 @@ fun CustomContent(padding: PaddingValues) {
         // Contenido de la aplicación
         content = {
             Spacer(Modifier.size(20.dp))
+            LazyRowKyara(characterList(), modifier = Modifier)
+            Spacer(Modifier.size(20.dp))
             LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
             ) {
                 val listica = mutableListOf<Movie>()
                 listica.add(Movie("saving private azu-nyan", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
-                listica.add(Movie("saving private azu-nyan", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
-                listica.add(Movie("saving private azu-nyan", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
-                listica.add(Movie("saving private azu-nyan", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
+                listica.add(Movie("k-on! the movie", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
+                listica.add(Movie("k-on! college", "kakifly", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
+                listica.add(Movie("k-on!!", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
                 listica.add(Movie("saving private azu-nyan", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
                 listica.add(Movie("saving private azu-nyan", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
                 listica.add(Movie("saving private azu-nyan", "naoko yamada", "https://i.pinimg.com/736x/9b/0f/82/9b0f823ea5aac967326634839834827d.jpg"))
@@ -232,6 +163,8 @@ fun CustomBottomBar() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTopBar() {
+    var displayrow = false
+    val haptics = LocalHapticFeedback.current
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
             containerColor = colorScheme.primaryContainer,
@@ -252,8 +185,9 @@ fun CustomTopBar() {
                 )
             }
         },
+
         actions = {
-            IconButton(onClick = { /* do something */ }) {
+            IconButton(onClick = {}) {
                 Icon(
                     imageVector = Icons.Filled.Home,
                     contentDescription = "Localized description"
@@ -278,7 +212,7 @@ fun MovieRow(model: Movie) {
             .padding(20.dp)
             .border(1.dp, colorScheme.primary, shape = RoundedCornerShape(20.dp))
             .fillMaxWidth()
-            .background(colorScheme.tertiaryContainer, shape = RoundedCornerShape(20.dp)),
+            .background(colorScheme.primaryContainer, shape = RoundedCornerShape(20.dp)),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         AsyncImage(
@@ -288,9 +222,8 @@ fun MovieRow(model: Movie) {
                 .padding(10.dp)
                 .width(100.dp)
                 .height(90.dp)
-                .align(Alignment.CenterVertically)
         )
-        Column(Modifier.align(Alignment.CenterVertically)){
+        Column(Modifier.align(Alignment.CenterVertically).fillMaxWidth().padding(horizontal = 20.dp)){
             Text(text=model.name,
                 style = MaterialTheme.typography.titleSmall
             )
@@ -298,6 +231,51 @@ fun MovieRow(model: Movie) {
             Text(text=model.director,
                 style = MaterialTheme.typography.bodySmall
             )
+        }
+    }
+}
+
+data class Character (val link: String, val name: String)
+fun characterList(): List<Character> {
+    return listOf(
+        Character("https://static.wikia.nocookie.net/k-on/images/5/58/Yui_Icon.png/revision/latest/scale-to-width-down/250?cb=20140723211155", "yui"),
+        Character("https://static.wikia.nocookie.net/k-on/images/a/a4/Mio_Icon.png/revision/latest/scale-to-width-down/250?cb=20140723211223", "mio"),
+        Character("https://static.wikia.nocookie.net/k-on/images/7/73/Tsumugi_Icon.png/revision/latest/scale-to-width-down/250?cb=20140723211921", "mugi"),
+        Character("https://static.wikia.nocookie.net/k-on/images/7/7a/Ritsu_Icon.png/revision/latest/scale-to-width-down/250?cb=20140723211412", "ricchan"),
+        Character("https://static.wikia.nocookie.net/k-on/images/6/61/Azusa_Icon.png/revision/latest/scale-to-width-down/250?cb=20140723212332", "azu-nyan"),
+        Character("https://i.pinimg.com/236x/cf/a1/16/cfa116ce0de13679a78345a8f824b8e6.jpg", "sawa-chan"),
+        Character("https://static.wikia.nocookie.net/k-on/images/7/7b/Nodoka_newshot.png/revision/latest?cb=20140723210851", "nodoka")
+    )
+}
+
+@Composable
+fun KyaraCard(kyara: Character, modifier: Modifier) {
+    Card(modifier = modifier) {
+        Column(
+            modifier = Modifier.padding(16.dp).size(140.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            AsyncImage(
+                model = kyara.link,
+                contentDescription = kyara.name,
+                modifier = Modifier
+                    .padding(6.dp).size(90.dp).clip(CircleShape)
+            )
+            Text(
+                text = kyara.name,
+                modifier = Modifier.padding(bottom = 3.dp),
+                fontWeight = FontWeight.Bold,
+            )
+        }
+    }
+}
+
+@Composable
+fun LazyRowKyara(kyaraList: List<Character>, modifier: Modifier) {
+    LazyRow(modifier = modifier) {
+        items(kyaraList) { kyara ->
+            KyaraCard(kyara = kyara, modifier = Modifier.padding(8.dp))
         }
     }
 }
